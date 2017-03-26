b6d6261ac3634d0901a1a12e834ccbc55bac357c
Colin Marsch <colinmarsch@gmail.com>
2017-03-26 00:34:32 -0400

    Ended dialog

--- messages/index.js
+++ messages/index.js
@@ -48,6 +48,7 @@ bot.dialog('/greet', [
     } else {
       session.send("Hi, %s. How are you doing?", session.userData.name);
     }
+    session.endDialog();
   }
 ]);
 
@@ -66,6 +67,7 @@ bot.dialog('/numbers', [
   function(session) {
     session.send("Here are some great 24/7 hotlines in the Waterloo region: - Supportive and Confidential Listening (519-745-1166), Here 24/7: Addictions, Mental Health & Crisis Services (1-844-437-3247), Good2Talk Support Line for Post-secondary Students (1-866-925-5454), 24-hour Support Line for Sexual Violence Survivors (519-741-8633), Mental Health and Community Referral Information (519-744-5594).");
     session.send("I'm so glad you talked to me about this. Remember, being aware of how you're feeling is a huge first step. Keep going and don't give up, you got this!");
+    session.endDialog();
   }]);
 
 
