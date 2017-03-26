a6d58d71f5180be963f4f10c3ae77fd3158c5732
the-ericwang35 <the.ericwang35@gmail.com>
2017-03-25 23:47:10 -0400

    Added requestNumber

---
+++ index.js
@@ -0,0 +1,106 @@
+Skip to content
+This repository
+Search
+Pull requests
+Issues
+Gist
+ @the-ericwang35
+ Sign out
+ Watch 2
+  Star 0
+  Fork 0 the-ericwang35/WaterlooTalks
+ Code  Issues 0  Pull requests 0  Projects 0  Wiki  Pulse  Graphs  Settings
+Branch: master Find file Copy pathWaterlooTalks/messages/index.js
+282f104  9 minutes ago
+@colinmarsch colinmarsch Fixing bugs regarding greeting
+1 contributor
+RawBlameHistory     
+Executable File  78 lines (65 sloc)  2.77 KB
+/*-----------------------------------------------------------------------------
+This template demonstrates how to use an IntentDialog with a LuisRecognizer to add
+natural language support to a bot.
+For a complete walkthrough of creating this type of bot see the article at
+http://docs.botframework.com/builder/node/guides/understanding-natural-language/
+-----------------------------------------------------------------------------*/
+"use strict";
+var builder = require("botbuilder"); // require the botbuilder module
+var botbuilder_azure = require("botbuilder-azure");
+
+var useEmulator = (process.env.NODE_ENV == 'development');
+
+// build the connector
+var connector = useEmulator ? new builder.ChatConnector() : new botbuilder_azure.BotServiceConnector({
+    appId: process.env['MicrosoftAppId'],
+    appPassword: process.env['MicrosoftAppPassword'],
+    stateEndpoint: process.env['BotStateEndpoint'],
+    openIdMetadata: process.env['BotOpenIdMetadata']
+});
+
+// create the bot
+var bot = new builder.UniversalBot(connector);
+
+// Make sure you add code to validate these fields
+var luisAppId = process.env.LuisAppId;
+var luisAPIKey = process.env.LuisAPIKey;
+var luisAPIHostName = process.env.LuisAPIHostName || 'westus.api.cognitive.microsoft.com';
+
+const LuisModelUrl = 'https://' + luisAPIHostName + '/luis/v1/application?id=' + luisAppId + '&subscription-key=' + luisAPIKey;
+
+// Main dialog with LUIS
+var recognizer = new builder.LuisRecognizer(LuisModelUrl);
+var intents = new builder.IntentDialog({ recognizers: [recognizer] });
+/*
+.matches('<yourIntent>')... See details at http://docs.botframework.com/builder/node/guides/understanding-natural-language/
+*/
+
+// start the dialog (chat session)
+bot.dialog('/', intents);
+
+intents.matches('greeting', '/greet');
+intents.matches('requestNumber', '/number');
+
+bot.dialog('/greet', [
+  function(session, args, next) {
+    if(!session.userData.name) {
+      session.beginDialog('/profile');
+    } else {
+      session.send("Hi, %s. How are you doing?", session.userData.name);
+    }
+  }
+]);
+
+bot.dialog('/profile', [
+  function(session) {
+    builder.Prompts.text(session, "Hey there! What is your name?");
+  },
+  function(session, results) {
+    session.userData.name = results.response;
+    session.send("Hi, %s. How are you doing?", session.userData.name);
+    session.endDialog();
+  }
+]);
+
+bot.dialog('/numbers', [
+  function(session) {
+    session.send("Here are some great 24/7 hotlines in the Waterloo region: - Supportive and Confidential Listening (519-745-1166), Here 24/7: Addictions, Mental Health & Crisis Services (1-844-437-3247), Good2Talk Support Line for Post-secondary Students (1-866-925-5454), 24-hour Support Line for Sexual Violence Survivors (519-741-8633), Mental Health and Community Referral Information (519-744-5594).");
+    session.send("I'm so glad you talked to me about this. Remember, being aware of how you're feeling is a huge first step. Keep going and don't give up, you got this!");
+  }]);
+
+
+
+intents.onDefault((session) => {
+    session.send('Sorry, I did not understand \'%s\'.', session.message.text);
+});
+
+if (useEmulator) {
+    var restify = require('restify');
+    var server = restify.createServer();
+    server.listen(3978, function() {
+        console.log('test bot endpont at http://localhost:3978/api/messages');
+    });
+    server.post('/api/messages', connector.listen());
+} else {
+    module.exports = { default: connector.listen() }
+}
+Contact GitHub API Training Shop Blog About
+© 2017 GitHub, Inc. Terms Privacy Security Status Help
