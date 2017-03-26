e12321a69df52e7c79e61619d9d75c0669d467f0
Jason Long <jasonlong98@hotmail.com>
2017-03-26 04:19:19 -0400

    changed url for api request

--- messages/index.js
+++ messages/index.js
@@ -77,7 +77,7 @@ bot.dialog('/feeling', [
   function(session) {
     var ourRequest = new XMLHttpRequest();
     var res = session.message.text.replace(" ", "+");
-    ourRequest.open('GET', 'https://api.datamarket.azure.com/data.ashx/amla/text-analytics/v1/GetSentiment?Text=' + res);
+    ourRequest.open('GET', 'https://westus.api.cognitive.microsoft.com/text/analytics/v2.0/sentiment?' + res;
     ourRequest.onload = function(){
       if (ourRequest.status >= 200 & ourRequest.status < 400) { //check if connection was successful
         var data = JSON.parse(ourRequest.responseText);
