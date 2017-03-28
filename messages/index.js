754e7e64503d86b3517066c25e31da119f77d5df
Jason Long <jasonlong98@hotmail.com>
2017-03-28 04:37:41 -0400

    deleted old javascript http request code

--- messages/index.js
+++ messages/index.js
@@ -78,32 +78,7 @@ bot.dialog('/profile', [
 
 bot.dialog('/feeling',
   function(session) {
-    //var ourRequest = new XMLHttpRequest();
     var res = session.message.text.replace(/ /g, "+");
-    /*var params = JSON.stringify({ text : `${res}` });
-    ourRequest.open('GET', 'https://westus.api.cognitive.microsoft.com/text/analytics/v2.0/sentiment?');
-    ourRequest.setRequestHeader("Content-Type","application/json");
-    ourRequest.setRequestHeader("Ocp-Apim-Subscription-Key","88d91d2cc28c48628da9256371be038e");
-    ourRequest.onload = function(){
-      if (ourRequest.status >= 200 && ourRequest.status < 400) { //check if connection was successful
-        var data = JSON.parse(ourRequest.responseText);
-        if(data.documents[0].score < 0.3){
-          session.beginDialog('/promptSad');
-        } else {
-          session.beginDialog('/promptHappy');
-        }
-      } else {
-        console.log("The server returned an error");
-        session.send("server error");
-      }
-    };
-    ourRequest.onerror = function(){
-      console.log("There was an error");
-      session.send("error");
-    };
-    ourRequest.send(params);
-    session.endDialog();
-  }*/
     unirest.post('https://westus.api.cognitive.microsoft.com/text/analytics/v2.0/sentiment?')
     .headers({'Accept': 'application/json', 'Content-Type': 'application/json', 'Ocp-Apim-Subscription-Key': '88d91d2cc28c48628da9256371be038e'})
     .send({ "documents": [{"language": "en", "id": "bot", "text": res}]})
